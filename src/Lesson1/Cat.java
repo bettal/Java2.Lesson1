1aded63d50b973021aaf7dc78a16467f44cef824
bettal <42188786+bettal@users.noreply.github.com>
2018-12-22 14:09:25 +0300

    Add cat info

---
+++ src/Lesson1/Cat.java
@@ -0,0 +1,17 @@
+package Lesson1;
+
+public class Cat {
+    String name;
+    String color;
+    int age;
+
+    public Cat(String name, String color, int age) {
+        this.name = name;
+        this.color = color;
+        this.age = age;
+    }
+
+    public void info(){
+        System.out.println( name + " " + color + " " + age );
+    }
+}
