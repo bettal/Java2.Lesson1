58b06e5e7762bcd8a42c4b21ae31dd2fa2f053b1
bettal <42188786+bettal@users.noreply.github.com>
2018-12-22 19:24:18 +0300

    Add setAge add getAge

--- src/Lesson1/Cat.java
+++ src/Lesson1/Cat.java
@@ -8,11 +8,22 @@ public class Cat {
     public Cat(String name, String color, int age) {
         this.name = name;
         this.color = color;
-        this.age = age;
+        this.age = getAge(age);
 
     }
 
     public void info(){
         System.out.println( name + " " + color + " " + age );
+    }
+
+    public int getAge(){
+        return age;
+    };
+    public void setAge(int age) {
+        if(age >=0) {
+            this.age = age;
+        }else{
+            System.out.println("Возрасть не можеть меньше нуля!");
+        }
     }
 }
