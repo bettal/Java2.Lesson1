0da56e2590c9490f431ecb03224c962b01cc4354
bettal <42188786+bettal@users.noreply.github.com>
2018-12-23 23:17:05 +0300

    Add maraphon and class Competitor

---
+++ src/Lesson1/maraphon/Competitor.java
@@ -0,0 +1,9 @@
+package Lesson1.maraphon;
+
+public interface Competitor {
+    void run (int dist);
+    void swim (int dist);
+    void jump (int height);
+    boolean isDistance ();
+    void info();
+}
